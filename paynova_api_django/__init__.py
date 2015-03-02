c773676238e98596652a784e0824f013e47af93c
Andrey Kolpakov <aakolpakov@gmail.com>
2015-03-02 10:06:07 +0100

    yo python-package

---
+++ paynova_api_django/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of paynova-api-django.
+# https://github.com/akolpakov/paynova-api-django
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2015, Andrey Kolpakov <aakolpakov@gmail.com>
+
+from paynova_api_django.version import __version__  # NOQA
